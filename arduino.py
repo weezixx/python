9f5af068f9e70c411ca2a1b95fdfce90edf8f8bf
weezixx <weezixx@hotmail.com>
2017-03-01 05:55:50 +0100

    version 1.3

--- arduino.py
+++ arduino.py
@@ -12,7 +12,7 @@ import os
 maliste = []
 inventaire = open("C:/Users/Kevin/Desktop/ie/tfe/code/python/magasin.txt",'r')
 
-#Comment mettre a jour mon code sur github via pycharm ????
+#Comment mettre a jour mon code sur github via pycharm ??????
 
 nom = ""
 coord_x = 0
